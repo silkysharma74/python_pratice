246c9e5e66d12767c84f6cfb3c8d572379c172a1
Silky Sharma <121107166+silkysharma74@users.noreply.github.com>
2025-08-25 15:04:37 +0530

    Update multiplication_table.py

--- .github/multiplication_table.py
+++ .github/multiplication_table.py
@@ -1,2 +1,12 @@
 6) Write a function that takes an integer number as input and prints its multiplication table.return type is  void.
+def print_table(num):
+    for i in range(1,11):
+        print(num, "x", i, "=", num * i)
+
+def main():
+        n = int(input("Enter a integer: "))
+        print_table(n)
+    
+
+main()
 
