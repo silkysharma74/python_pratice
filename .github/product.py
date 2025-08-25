3dfb3c0bec59abf43c1f031bb5e45d721a0c2378
Silky Sharma <121107166+silkysharma74@users.noreply.github.com>
2025-08-25 13:29:46 +0530

    Create product.py

---
+++ .github/product.py
@@ -0,0 +1,12 @@
+def product(a, b):
+    return a * b
+
+def main():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    result = product(a, b)
+  
+    print("Product is:", result) 
+
+main()
