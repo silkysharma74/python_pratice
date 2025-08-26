01c77fab4af31a730bdf578a5333eb8ed6adf818
Silky Sharma <121107166+silkysharma74@users.noreply.github.com>
2025-08-26 15:40:51 +0530

    Update month_days.py

--- .github/month_days.py
+++ .github/month_days.py
@@ -2,17 +2,17 @@
 
 
 def get_days(month, year):
-    if month in [1,3,5,7,8,10,12]:
+    if month in [1,3,5,7,8,10,12]: 
         return 31
-    elif month in [4,6,9,11]:
+    elif month in [4,6,9,11]: 
         return 30
     elif month == 2:
         if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
             return 29
         else:
             return 28
-    else:
-        return -1   
+    else: 
+        return -1  
         
 def main():
     
